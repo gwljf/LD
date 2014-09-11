50852b5cb83b9d7736ffba984824b0ecaec79a08
gwljf <906642458@qq.com>
2014-09-11 14:42:39 -0700

    RandomListNode

---
+++ src/Basic_Structue/RandomListNode.java
@@ -0,0 +1,10 @@
+package Basic_Structue;
+
+/**
+ * Created by wlf on 9/11/14.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next, random;
+    RandomListNode(int x) { this.label = x; }
+}
