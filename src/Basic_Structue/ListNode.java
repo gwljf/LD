3dde3b3369bb9799e3d79ee65fb9bd812b0f4c4b
gwljf <906642458@qq.com>
2014-09-19 16:54:42 -0700

    add create function and print function

--- src/Basic_Structue/ListNode.java
+++ src/Basic_Structue/ListNode.java
@@ -11,4 +11,28 @@ public class ListNode {
         this.val = _val;
         next = null;
     }
+
+    public static ListNode create(int[] array) {
+        if (array.length==0){
+            return null;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        for (int ele:array){
+            cur.next = new ListNode(ele);
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    public static void print(ListNode head){
+        ListNode cur = head;
+        while (cur!=null){
+            if (cur.next==null){
+                System.out.print(cur.val+"");
+            }else {
+                System.out.print(cur.val+"->");
+            }
+        }
+    }
 }
