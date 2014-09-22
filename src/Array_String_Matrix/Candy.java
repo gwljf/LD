ef8dd8b55317140677671e5ecc9e6e41cef526ea
gwljf <906642458@qq.com>
2014-09-22 15:58:13 -0700

    Candy

---
+++ src/Array_String_Matrix/Candy.java
@@ -0,0 +1,43 @@
+package Array_String_Matrix;
+
+/**
+ * Created by wlf on 9/22/14.
+ */
+public class Candy {
+    public static void main (String[] args) {
+        int[] ratings = {1,2};
+        System.out.println(candy(ratings));
+    }
+
+    public static int candy(int[] ratings) {
+        int len = ratings.length;
+        if(len==0 || len==1){
+            return len;
+        }
+
+        int[] candy = new int[len];
+        int cur = 1;
+        for(int i=1; i<len; ++i){
+            if(ratings[i]>ratings[i-1]){        // 后面的比前面杜大
+                cur++;
+            } else{
+                cur = 1;
+            }
+            candy[i] = cur;
+        }
+        cur = 1;
+        for(int i=len-2; i>=0; --i){
+            if(ratings[i]>ratings[i+1]){            // 前面杜比后面的大
+                cur++;
+            }else{
+                cur = 1;
+            }
+            candy[i] = Math.max(cur, candy[i]);
+        }
+        int min = 0;
+        for(int i=0; i<len; ++i){
+            min += candy[i];
+        }
+        return min;
+    }
+}
