76c575b392ee62c683008585e45eaf6013a776ae
gwljf <906642458@qq.com>
2014-10-06 16:55:42 -0700

    Gas station

--- src/Array_String_Matrix/Gas_Station.java
+++ src/Array_String_Matrix/Gas_Station.java
@@ -4,6 +4,18 @@ package Array_String_Matrix;
  * Created by wlf on 9/22/14.
  */
 public class Gas_Station {
+
+/*
+    There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+    You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+
+    Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+    Note:
+    The solution is guaranteed to be unique.
+*/
+
     public static int canCompleteCircuit2(int[] gas, int[] cost) {
         int len = gas.length;
         int remain = 0;
