c04a2918fb55a71ca3e190d7d94238cde3daa8f3
gwljf <906642458@qq.com>
2014-10-06 17:17:04 -0700

    Longest Consecutive Sequence

--- src/Array_String_Matrix/Longest_Consecutive_Sequence.java
+++ src/Array_String_Matrix/Longest_Consecutive_Sequence.java
@@ -7,6 +7,18 @@ import java.util.Set;
  * Created by wlf on 9/17/14.
  */
 public class Longest_Consecutive_Sequence {
+/*
+
+    Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+    For example,
+    Given [100, 4, 200, 1, 3, 2],
+    The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+
+    Your algorithm should run in O(n) complexity.
+*/
+
+
     public int longestConsecutive(int[] num) {
         if (num.length==0) {
             return 0;
