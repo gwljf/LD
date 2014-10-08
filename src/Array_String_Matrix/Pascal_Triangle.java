301661930bdbe521e1684aa6f63a4ab466fede13
gwljf <906642458@qq.com>
2014-10-07 18:37:18 -0700

    Pascal's Triangle

--- src/Array_String_Matrix/Pascal_Triangle.java
+++ src/Array_String_Matrix/Pascal_Triangle.java
@@ -44,4 +44,31 @@ public class Pascal_Triangle {
         }
         return tree;
     }
+
+/*
+    Given an index k, return the kth row of the Pascal's triangle.
+
+    For example, given k = 3,
+    Return [1,3,3,1].
+
+    Note:
+    Could you optimize your algorithm to use only O(k) extra space?
+*/
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> layer = new ArrayList<Integer>();
+        if(rowIndex<0){
+            return layer;
+        }
+        layer.add(1);
+        for(int layerIndex=1; layerIndex<=rowIndex; ++layerIndex){
+            for(int index=layerIndex; index>0; --index){        // 这注意是大于0, 这样就不用考虑第一位是1了
+                if(index==layerIndex){
+                    layer.add(1);
+                } else {
+                    layer.set(index, layer.get(index)+layer.get(index-1));
+                }
+            }
+        }
+        return layer;
+    }
 }
