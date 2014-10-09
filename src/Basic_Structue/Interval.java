b36e569f0a88adde0cb05ed2c4be72dbac1f289b
gwljf <906642458@qq.com>
2014-10-08 17:04:41 -0700

    Interval

---
+++ src/Basic_Structue/Interval.java
@@ -0,0 +1,11 @@
+package Basic_Structue;
+
+/**
+ * Created by wlf on 10/8/14.
+ */
+public class Interval {
+    public int start;
+    public int end;
+    public Interval() { start = 0; end = 0; }
+    public Interval(int s, int e) { start = s; end = e; }
+}
