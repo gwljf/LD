c44ee5c8915991cfec07d7256288616abaa0bf9c
gwljf <906642458@qq.com>
2014-10-08 17:40:28 -0700

    Valid_Parentheses

---
+++ src/Array_String_Matrix/Valid_Parentheses.java
@@ -0,0 +1,51 @@
+package Array_String_Matrix;
+
+import java.util.Stack;
+
+/**
+ * Created by wlf on 10/8/14.
+ */
+public class Valid_Parentheses {
+
+/*
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+    The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*/
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0; i<s.length(); ++i){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                } else if(s.charAt(i)==')'){
+                    if(stack.peek()=='('){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                } else if(s.charAt(i)=='}'){
+                    if(stack.peek()=='{'){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                } else if(s.charAt(i)==']'){
+                    if(stack.peek()=='['){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if (stack.isEmpty()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
